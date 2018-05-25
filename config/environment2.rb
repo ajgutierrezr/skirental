534385a4dba90765115c14aab210f34648a503e6
ajgutierrezr <ajgutierrezr@gmail.com>
2018-05-25 11:59:23 -0400

    env

---
+++ config/environment2.rb
@@ -0,0 +1,15 @@
+# Load the Rails application.
+require_relative 'application'
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => '',
+  :password => ''
+  :domain => 'www.rentalskichile.cl',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+# Initialize the Rails application.
+Rails.application.initialize!
